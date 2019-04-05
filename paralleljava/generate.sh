79c99b0cf228ef05c15cdf899a280ff8d139266d
Jan Ouwens <jan.ouwens@gmail.com>
2019-04-05 15:08:02 +0200

    ParallelJava: tweaks the layout to support slide-level 2

--- paralleljava/generate.sh
+++ paralleljava/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pandoc --to=revealjs --standalone --variable revealjs-url=../../-revealjs-3.8.0 --css=../theme-modifications.css --highlight-style zenburn --output=src/index.html src/talk.md
+pandoc --to=revealjs --standalone --slide-level=2 --variable revealjs-url=../../-revealjs-3.8.0 --css=../theme-modifications.css --highlight-style zenburn --output=src/index.html src/talk.md
 
