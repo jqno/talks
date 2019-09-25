463343c901f8715f6777dc597a78a106baa95e3a
Jan Ouwens <jan.ouwens@gmail.com>
2019-09-25 09:15:41 +0200

    Script: separates watch and run scripts

---
+++ watch.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+PRESENTATION=$1
+if [ -z "$PRESENTATION" ]; then
+  echo "No presentation set"
+  exit 1
+fi
+
+chokidar "$PRESENTATION/**/*.md" "$PRESENTATION/**/*.css" -c "cd $PRESENTATION && ./generate.sh"
+
